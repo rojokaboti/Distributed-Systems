90f9700005152c5d74319c4f9865f898e28f4ded
rojokaboti <rojokaboti@gmail.com>
2024-01-05 23:19:13 +0300

    PA3-update4

--- Programming Assignment #3/Solution/paxos.py
+++ Programming Assignment #3/Solution/paxos.py
@@ -2,6 +2,27 @@ import sys
 import multiprocessing as mp
 import zmq
 
+
+def get_param():
+    # This function reads the argumets given in command line 
+    # and prints them at the end with some formatting
+    if len(sys.argv) != 4:
+        print("Usage: python3 paxos.py [number of nodes] [probability of a crash] [number of rounds]")
+        sys.exit(1)
+    try:
+        num_nodes = int(sys.argv[1])
+        crash_prob = float(sys.argv[2])
+        num_rounds = int(sys.argv[3])
+        round = 0
+        if crash_prob>1 or crash_prob<0:
+            print('The given crash probability is not valid!')
+        else:
+            print(f"NUM NODES: {num_nodes}, CRASH PROB: {crash_prob}, NUM ROUNDS: {num_rounds}")      
+    except:
+        print('One or more of the parameters is incorrect!')
+
+    return num_nodes, crash_prob, num_rounds
+
 class PaxosNode(ID, crash_prob, num_nodes, val, num_rounds):        
     def __init__(self,ID: int, prob:float , num_nodes:int, val:int , numRounds:int):
         self.ID = ID
@@ -14,6 +35,7 @@ class PaxosNode(ID, crash_prob, num_nodes, val, num_rounds):
         self.proposeVal = None
         self.decision = None
         self.leader = False
+        self.barrier = mp.Barrier(num_nodes)
         
 
     def network(self):
@@ -57,12 +79,16 @@ class PaxosNode(ID, crash_prob, num_nodes, val, num_rounds):
     def broadcast_failure(self, msg, proposer, num_nodes, prob):
         self.send(msg)
 
-    paxos_node = mp.Process(target=self.network, args=())
-
 
     def leader_process(self):
+        pull_socket, push_sockets = self.network()
         msg = f"ROUND {round} STARTED WITH INITIAL VALUE: {self.val}"
         self.broadcast_failure(msg, 1, self.num_nodes, self.prob)
+        self.barrier.wait()
+        for i in range(num_nodes):
+            self.recieve_msg()
+
+
 
        
     def START(self, round):
@@ -74,27 +100,12 @@ class PaxosNode(ID, crash_prob, num_nodes, val, num_rounds):
 
 
 if __name__ == "__main__":
+    #getting parameters from the command line.
+    num_nodes, crash_prob, num_rounds = get_param()
 
-# This part reads the argumets given in command line 
-# and prints them at the end with some formatting
-    if len(sys.argv) != 4:
-        print("Usage: python paxos.py [number of nodes] [probability of a crash] [number of rounds]")
-        sys.exit(1)
-    try:
-        num_nodes = int(sys.argv[1])
-        crash_prob = float(sys.argv[2])
-        num_rounds = int(sys.argv[3])
-        round = 0
-        if crash_prob>1 or crash_prob<0:
-            print('The given crash probability is not valid!')
-        else:
-            print(f"NUM NODES: {num_nodes}, CRASH PROB: {crash_prob}, NUM ROUNDS: {num_rounds}")      
-    except:
-        print('One or more of the parameters is incorrect!')
-
-    barrier = mp.Barrier(num_nodes)
     
-    paxos_nodes = [mp.Process(target=PaxosNode, args=(, i)) for i in range(num_processes)]
+    
+    paxos_nodes = [mp.Process(target=PaxosNode, args=(i)) for i in range(num_nodes)]
 
 
     print(paxos_nodes)
