c29f283de3d172ce34bd4a2bcedadc596cf1d82d
rojokaboti <rojokaboti@gmail.com>
2024-01-07 21:14:03 +0300

    PA3 update

--- PA3/Solution/paxos.py
+++ PA3/Solution/paxos.py
@@ -69,7 +69,7 @@ class PaxosNode:
             self.sendFailure (msg, proposer, target, prob)
 
 
-    def send(self, msg):
+    def send(self, msg, target):
         print(msg)
 
     
@@ -80,17 +80,107 @@ class PaxosNode:
         self.send(msg)
 
 
+
+"""It first broadcasts a START message using broadcastFailure method de-
+scribed above. Since the broadcast is done by broadcastFailure method,
+each acceptor might nondeterministically receive a START or "CRASH r%N"
+3
+message representing temporary crashes of the proposer. Since nodes are
+synchronized, you do not have to attach round number to the START mes-
+sage.
+• The proposer blocks for receiving N messages (including one from itself
+as well) and keeps the JOIN count for these messages. There are three
+types of possible incoming messages: They can start with JOIN, CRASH or
+START. Receiving its own START message is interpreted as a successful join
+to this round. If the proposer does not receive a START message, it receives
+a CRASH for its place which will interpreted as failure of this node as an
+acceptor as well. Note that the proposer always receives N messages in
+total.
+• If the total number of JOIN and START messages are bigger than N/2,
+then the proposer picks the JOIN message with the maxV otedRound field
+and sets the proposeV al as maxV otedV al of this message. If it has re-
+ceived a START message from itself, its own local maxV otedRound and
+maxV otedV al variables are taken into consideration for the computation
+of proposeV al. If the resulting maxV otedRound is −1 meaning that none
+of the nodes in the join quorum has voted before, then the proposer n sets
+proposeV al to val (input of the PaxosNode method). Then, it broadcasts
+"PROPOSE proposeV al" message using broadcastFailure method.
+• If the total number of JOIN and START messages are less than or equal to
+N/2, then the proposer broadcasts a ROUNDCHANGE message to all nodes
+and moves directly to the next round. ROUNDCHANGE message is sent for
+establishing the synchronization among nodes. Therefore, it must not be
+sent using the broadcastFailure and directly sent to all nodes (except itself)
+without any failure possibility.
+• If n has previously decided to send a PROPOSE message, it blocks for receiv-
+ing N messages. The possible messages it can get start with VOTE, CRASH
+or PROPOSE. If it receives its own PROPOSE message, it can be thought as a
+positive VOTE response to itself and then it updates the maxV otedRound
+to r and maxV otedV al to its own proposeV al. If total number of VOTE
+and PROPOSE messages are bigger than N/2, this round reaches to a de-
+cision and decision becomes proposeV al. After receiving N messages, n
+moves to the next round."""
     def leader_process(self):
         pull_socket, push_sockets = self.network()
         msg = f"ROUND {round} STARTED WITH INITIAL VALUE: {self.val}"
-        self.broadcast_failure(msg, 1, self.num_nodes, self.prob)
-        self.barrier.wait()
+        self.broadcast_failure(msg, 1, self.num_nodes, self.prob)  #fix this
+        joins = 0
+        self_join = False
         for i in range(num_nodes):
-            self.recieve_msg()
+            resp = self.recieve_msg()
+            if "JOIN" in resp:
+                joins += 1
+            elif "START" in resp:
+                joins += 1
+                self_join = True
+            else:
+                pass
+        if (joins > (self.num_nodes/2)) and (self_join == True):
+            pass
+        else:
+
+
+    def acceptor_process(self):
+        while True:
+            msg = self.recieve_msg()
+            break
+
+        if "START" in msg:
+            print(f"ACCEPTOR {self.ID} RECEIVED IN JOIN PHASE: START")
+            resp = f"JOIN {self.maxVotedRound} {self.maxVotedVal}"
+            proposer_id = round%self.num_nodes
+            self.sendFailure(resp, proposer_id, self.crash_prob)
+
+        elif "CRASH" in msg:
+            proposer_id = round%self.num_nodes
+            print(f"ACCEPTOR {self.ID} RECEIVED IN JOIN PHASE: CRASH {proposer_id}")
+            resp = f"CRASH {proposer_id}"
+            self.send(resp, proposer_id)
+
+        while True:
+            msg = self.recieve_msg()
+            break
+
+        if "PROPOSE" in msg:
+            print(f"ACCEPTOR {self.ID} RECEIVED IN VOTE PHASE: {msg}")
+            resp = "VOTE"
+            self.sendFailure(resp, proposer_id, self.crash_prob)
+            self.maxVotedRound = round
+            self.maxVotedVal = msg[1]
+            print(f"ACCEPTOR {self.ID} RECEIVED IN VOTE PHASE: ROUNDCHANGE")
+
+        elif "CRASH" in msg:
+            print(f"ACCEPTOR {self.ID} RECEIVED IN VOTE PHASE: {msg}")
+            resp = "CRASH {proposer_id}"
+            self.send(resp, proposer_id)
+            print(f"ACCEPTOR {self.ID} RECEIVED IN VOTE PHASE: ROUNDCHANGE")
+
+        else:
+            print(f"ACCEPTOR {self.ID} RECEIVED IN VOTE PHASE: ROUNDCHANGE")
 
 
 
-       
+
+
     def START(self, round):
         if round % self.num_nodes ==  self.ID:
             self.leader_process()
